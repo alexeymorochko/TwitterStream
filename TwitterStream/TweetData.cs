e21f4b46ed0ed83412c535d1875e1ca4dee6ef23
alexeymorochko <alexeymorochko@yahoo.com>
2022-10-25 07:27:56 -0400

    Initial commit

---
+++ TwitterStream/TweetData.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterStream
+{
+    internal class TweetData
+    {
+        [JsonProperty(PropertyName = "entities")]
+        public Entity Entity { get; set; }
+    }
+}
